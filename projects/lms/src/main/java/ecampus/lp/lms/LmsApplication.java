e6b7d42f1e19aed59248137f1f0024d683cb726c
Davide Innocenti <innocenti.davide@gmail.com>
2023-11-08 13:05:32 +0100

    add security configuration

--- projects/lms/src/main/java/ecampus/lp/lms/LmsApplication.java
+++ projects/lms/src/main/java/ecampus/lp/lms/LmsApplication.java
@@ -2,8 +2,9 @@ package ecampus.lp.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class LmsApplication {
 
 	public static void main(String[] args) {
