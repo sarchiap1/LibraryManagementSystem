7539b8ab0c677ce3ce0733e85e383f386860b2e3
Davide Innocenti <innocenti.davide@gmail.com>
2023-11-08 23:24:20 +0100

    fix schema.sql

--- projects/lms/src/main/java/ecampus/lp/lms/LmsApplication.java
+++ projects/lms/src/main/java/ecampus/lp/lms/LmsApplication.java
@@ -2,9 +2,10 @@ package ecampus.lp.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+/*(exclude={DataSourceAutoConfiguration.class})*/
 public class LmsApplication {
 
 	public static void main(String[] args) {
--- projects/lms/src/main/resources/schema.sql
+++ projects/lms/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
-create table if not exists user (
-    'id'  identity    not null,
-    'first_name'  varchar(255)    not null,
-    'last_name'   varchar(255)    not null,
-    'email'       varchar(255)    not null,
-    'password'    varchar(255)    not null
-)
+CREATE TABLE IF NOT EXISTS `user` (
+    `id`            INTEGER  PRIMARY KEY AUTO_INCREMENT,
+     `first_name`   VARCHAR(255) NOT NULL,
+     `last_name`    VARCHAR(255) NOT NULL,
+     `email`        VARCHAR(255) NOT NULL,
+     `password`     VARCHAR(255) NOT NULL
+);
